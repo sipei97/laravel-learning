9848b593859bd721aad15cd57c76347a74443020
zengsp <zengsp.mail@qq.com>
2020-01-05 12:54:58 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,22 @@
 <head>
   <meta charset="UTF-8">
   <title>@yield('title', 'Laravel Learning')</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-  @yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="/" class="navbar-brand">Laravel Learning</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
+
 </body>
 </html>

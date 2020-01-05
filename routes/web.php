2c5fd31562b08400e9bb85ae1f58eec80bee85e9
zengsp <zengsp.mail@qq.com>
2020-01-05 21:07:17 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,24 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::prefix('todolist')->namespace('Todolist')->group(function () {
-    Route::prefix('index')->group(function () {
-        Route::get('index', 'IndexController@index');
-        Route::get('create', 'IndexController@create');
-        Route::post('store', 'IndexController@store');
-        Route::get('edit/{id}', 'IndexController@edit');
-        Route::post('update/{id}', 'IndexController@update');
-        Route::get('delete/{id}', 'IndexController@delete');
-        Route::get('destroy/{id}', 'IndexController@destroy');
-        Route::get('restore/{id}', 'IndexController@restore');
-        Route::get('forceDelete/{id}', 'IndexController@forceDelete');
-    });
-});
+// Route::prefix('todolist')->namespace('Todolist')->group(function () {
+//     Route::prefix('index')->group(function () {
+//         Route::get('index', 'IndexController@index');
+//         Route::get('create', 'IndexController@create');
+//         Route::post('store', 'IndexController@store');
+//         Route::get('edit/{id}', 'IndexController@edit');
+//         Route::post('update/{id}', 'IndexController@update');
+//         Route::get('delete/{id}', 'IndexController@delete');
+//         Route::get('destroy/{id}', 'IndexController@destroy');
+//         Route::get('restore/{id}', 'IndexController@restore');
+//         Route::get('forceDelete/{id}', 'IndexController@forceDelete');
+//     });
+// });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');

6ccee3c05b5a3e67245234693f67797a51f78e7b
zengsp <zengsp.mail@qq.com>
2020-01-07 15:16:24 +0000

    用户注册表单

--- database/migrations/2019_10_17_224242_create_todolists_table.php
+++ database/migrations/2019_10_17_224242_create_todolists_table.php
@@ -28,6 +28,6 @@ class CreateTodolistsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todos');
+        Schema::dropIfExists('todolists');
     }
 }

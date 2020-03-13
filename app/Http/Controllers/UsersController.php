e755bde3b5c11dfd22aef7059beeef2715e5214a
zengsp <zengsp.mail@qq.com>
2020-03-13 08:59:59 +0000

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -60,13 +60,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'zengsp.mail@qq.com';
-        $name = 'Zengsp';
         $to = $user->email;
         $subject = '感谢注册应用！请确认您的邮箱。';
 
-        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
